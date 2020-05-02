aaf643ecaa6bd4e02f52909a1b30cf31bda3dbe2
thms200 <thms0504@gmail.com>
2020-05-03 03:51:10 +0900

    feat: Add demo page

--- src/constants/message.js
+++ src/constants/message.js
@@ -6,4 +6,7 @@ export const MESSAGE = {
   INVALID_OFF: '이미 상담을 시작하였습니다. 상담이 종료 된 후 상담 모드를 종료할 수 있습니다.',
   NO_CUSTOMER: '대기 중인 고객이 없습니다.',
   REQUEST_PERMISSION: '카메라와 마이크 접근 권한을 허락해주세요!',
+  MODAL_INFO_NAME: '고객이 입력한 이름과 이메일이 유효한지 확인합니다.',
+  MODAL_INFO_MODE: '고객이 원하는 상담 모드에 맞춰 상담이 연결됩니다.',
+  MODAL_INFO_END: '상담 완료 후 종료하면 상담연결도 함께 종료됩니다.'
 };
--- src/containers/DemoContainer.js
+++ src/containers/DemoContainer.js
@@ -1,14 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import ModeModal from '../components/ModeModal';
+import ConsultingModal from '../components/ConsultingModal';
+import { MESSAGE } from '../constants/message';
 
 const Wrapper = styled('div')`
   display: flex;
+  font-family: 'Gamja Flower', cursive;
+
+  button {
+    background-color: transparent;
+    font-family: 'Gamja Flower', cursive;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
+const Chatbot = styled('button')`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border: none;
+  background-color: transparent;
+`;
+
+const InfoWrapper = styled('div')`
+  margin: 100px 0 0 50px;
+  font-family: 'PT Sans', sans-serif;
+`;
+
+const InfoRow = styled('div')`
+  margin: 50px 0;
+`;
+
+const Info = styled('h3')`
+  color: #888484;
+  transition-duration: 2s;
 `;
 
 export default function DemoContainer() {
+  const [isModeShow, setIsModeShow] = useState(false);
+  const [isConsultingShow, setIsConsultingShow] = useState(false);
+
+  const toggleSelectMode = () => setIsModeShow(!isModeShow);
+  const toggleConsulting = () => setIsConsultingShow(!isConsultingShow);
+  const requestConsulting = () => {
+    toggleSelectMode();
+    toggleConsulting();
+    alert(MESSAGE.MODAL_INFO_MODE);
+  };
+
   return (
     <Wrapper>
-      <div>Demo Page</div>
+      <InfoWrapper>
+        <h1>체험해보세요!</h1>
+        <Info>하단 오른쪽에 있는 채팅 버튼을 클릭해보세요!</Info>
+        {isModeShow && <InfoRow>
+          <Info>고객이 원하는 상담 모드를 선택할 수 있어요!</Info>
+          <Info>Camera 혹은 Voice를 클릭해보세요!</Info>
+        </InfoRow>}
+        {isConsultingShow && <InfoRow>
+          <Info>고객이 선택한 모드로 상담을 진행하시면됩니다!</Info>
+        </InfoRow>}
+      </InfoWrapper>
+      <Chatbot onClick={toggleSelectMode}>
+        <img
+          src="https://img.icons8.com/nolan/96/chat.png"
+          alt="chatBot"
+        />
+      </Chatbot>
+      {isModeShow && <ModeModal
+        onToggleSelectMode={toggleSelectMode}
+        onRequestConsulting={requestConsulting}
+      />}
+      {isConsultingShow && <ConsultingModal
+        onToggleConsulting={toggleConsulting}
+      />}
     </Wrapper>
   );
 }
