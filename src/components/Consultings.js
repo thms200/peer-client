23d56edb435c045ae11e18427e86c65539066415
thms200 <thms0504@gmail.com>
2020-05-02 12:56:02 +0900

    style: Modify card UI

--- src/components/Consultings.js
+++ src/components/Consultings.js
@@ -4,29 +4,34 @@ import PropTypes from 'prop-types';
 import { FaCamera, FaMicrophone } from 'react-icons/fa';
 
 const Wrapper = styled('section')`
+  width: 85%;
+  height: 91vh;
+  margin-top: 9vh;
+`;
+
+const FlexWrapper = styled('div')`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
-  width: 70%;
-  height: 100%;
-  margin-top: 9vh;
-  padding: 1vh 6vh;
+  margin: 0 auto;
 `;
 
 const ConsultingsBoxWrapper = styled('ul')`
-  width: 40%;
-  height: 20%;
-  margin: 10px;
-  padding: 5px 10px;
+  width: 28%;
+  height: 170px;
+  margin: calc(16% / 6);
+  padding: 10px;
   border-radius: 10px;
   border: 0.8px solid grey;
+  box-sizing: border-box;
   list-style: none;
   box-shadow: 2px 2px 5px 0px black;
+  background-color: white;
 `;
 
 const ConsultingsBox = styled('li')`
   color: #b0a9a9;
   font-size: 15px;
+  margin: 5px;
 
   ${props => {
     if (props.name) {
@@ -37,13 +42,23 @@ const ConsultingsBox = styled('li')`
 
 const ConsultingBoxName = styled('div')`
   color: rgb(105, 115, 251);
-  font-size: 35px;
+  font-size: 28px;
+`;
+
+const ConsultingBoxIcon = styled('div')`
+  width: 8%;
+  height: 25%;
+  border-radius: 50%;
+  padding: 5px;
+  text-align: center;
+  line-height: 20%;
+  background-color: #4c4848;
 `;
 
 const Audio = styled('audio')`
   width: 100%;
   height: 30px;
-  margin-top: 8px;
+  margin-top: 15px;
   &:focus {
     outline: none;
   }
@@ -51,29 +66,31 @@ const Audio = styled('audio')`
 
 const makeConsultingMode = (isVoice) => {
   return isVoice
-    ? <FaMicrophone color={'rgb(105, 115, 251)'} size={25} />
-    : <FaCamera color={'rgb(105, 115, 251)'} size={25} />;
+    ? <ConsultingBoxIcon><FaMicrophone size={18} color="white" /></ConsultingBoxIcon>
+    : <ConsultingBoxIcon><FaCamera size={18} color="white" /></ConsultingBoxIcon>;
 };
 
 export default function Consultings({ consultings }) {
   return (
     <Wrapper>
-      {consultings && consultings.map((consulting) => {
-        const { name, email, timestamp, audio, isVoice } = consulting;
-        const date = new Date(timestamp).toString().split('GMT')[0].trim();
-        return (
-          <ConsultingsBoxWrapper key={timestamp}>
-            <ConsultingsBox name={name}>
-              <ConsultingBoxName>{name}</ConsultingBoxName>
-              {makeConsultingMode(isVoice)}
-            </ConsultingsBox>
-            <ConsultingsBox>{email}</ConsultingsBox>
-            <ConsultingsBox>{date}</ConsultingsBox>
-            <ConsultingsBox>{isVoice}</ConsultingsBox>
-            <Audio src={audio} controls />
-          </ConsultingsBoxWrapper>
-        );
-      })}
+      <FlexWrapper>
+        {consultings && consultings.map((consulting) => {
+          const { name, email, timestamp, audio, isVoice } = consulting;
+          const date = new Date(timestamp).toString().split('GMT')[0].trim();
+          return (
+            <ConsultingsBoxWrapper key={timestamp}>
+              <ConsultingsBox name={name}>
+                <ConsultingBoxName>{name}</ConsultingBoxName>
+                {makeConsultingMode(isVoice)}
+              </ConsultingsBox>
+              <ConsultingsBox>{email}</ConsultingsBox>
+              <ConsultingsBox>{date}</ConsultingsBox>
+              <ConsultingsBox>{isVoice}</ConsultingsBox>
+              <Audio src={audio} controls />
+            </ConsultingsBoxWrapper>
+          );
+        })}
+      </FlexWrapper>
     </Wrapper>
   );
 }
